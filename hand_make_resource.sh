f5ebd0827dbb1eb4113dff7ca2bd2316e74e389e
gameKnife <kaimingyi@163.com>
2015-08-15 10:24:15 +0800

    add macosx toochain, pvrtextool new version

---
+++ hand_make_resource.sh
@@ -0,0 +1,10 @@
+cd exec/tools/global_task
+sh unzip_media_pack.sh
+cd ../resource_task
+PWD
+#call _obj2gmf_media.bat
+#call desktop_tga2dds_media.bat
+#call sgx_tga2pvr_media.bat
+#call adreno_tga2atc_media.bat
+
+#pause
