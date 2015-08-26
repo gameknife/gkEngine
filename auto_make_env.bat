fcd003c71d83fc2971c5d02bdca3740ea94740e5
gameKnife <kaimingyi@163.com>
2015-08-26 10:03:57 +0800

    complete the windows bat script

--- auto_make_env.bat
+++ auto_make_env.bat
@@ -1,5 +1,9 @@
 cd exec\tools\global_task
 
+set pauseprogress=1
+
+call set_global_env.bat
+
 rem step01, download, extract, place the denpendencys
 
 call extract_thirdparty.bat
@@ -15,9 +19,20 @@ call extract_media_pack.bat
 
 rem step4, auto build, if possible
 
+set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 
+IF EXIST %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe (
+echo MSBuild finded, build gkENGINE automaticly.
+call %msBuildDir%\msbuild.exe %GKENGINE_HOME%\..\gkENGINE_CORE_Vc10.sln /p:Configuration=Develop /p:Platform=Win32
+) ELSE (
+echo MSBuild Can not find, you should build gkENGINE manmally.
+)
 
 rem strp5, auto run, if possible
+
+IF EXIST %GKENGINE_HOME%\bin32\gkLauncherUniverse.exe (
+call %GKENGINE_HOME%\bin32\gkLauncherUniverse.exe
+)
 
 echo Press Any Key to Continue...
 
--- build_all.bat
+++ build_all.bat
@@ -1,11 +1,15 @@
-
 @echo off
-rem msbuild gkENGINE_CORE_Vc10.sln /t:rebuild
-rem SBuild.exe C:\Folder Containing My Visual Studio Solution\My Solution.sln /property:Configuration=Develop
+
+chcp 936
 
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-REM perhaps you need a different framework? %WINDIR%\Microsoft.NET\Framework\v4.0.30319
-rem call %msBuildDir%\msbuild.exe /help
+
+IF EXIST %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe (
+echo MSBuild finded, build gkENGINE automaticly.
 call %msBuildDir%\msbuild.exe gkENGINE_CORE_Vc10.sln /p:Configuration=Develop /p:Platform=Win32
-rem /target:AllTargetsWrapper "MySolutionOrCsProj.sln" /p:Configuration=Debug;FavoriteFood=Twix /l:FileLogger,Microsoft.Build.Engine;logfile=ZZZZZMSBuildSetupAndBuildAllTargetsWrapper_Debug.log
+) ELSE (
+echo MSBuild Can not find, you should build gkENGINE manmally.
+)
+
+
 PAUSE
