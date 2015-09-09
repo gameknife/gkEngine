6ed777796661fb16ef12df8daaca2638df52c15e
gameKnife <kaimingyi@163.com>
2015-09-10 00:19:27 +0800

    move sln to code folder

--- auto_make_env.bat
+++ auto_make_env.bat
@@ -53,7 +53,7 @@ set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 IF EXIST %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe (
 echo MSBuild finded, build gkENGINE automaticly...
 echo -------------------------------------------------------------------------
-call %msBuildDir%\msbuild.exe %GKENGINE_HOME%\..\gkENGINE_CORE_Vc10.sln /p:Configuration=Develop /p:Platform=Win32
+call %msBuildDir%\msbuild.exe %GKENGINE_HOME%\..\code\gkENGINE_CORE_Vc10.sln /p:Configuration=Develop /p:Platform=Win32
 ) ELSE (
 echo MSBuild Can not find, you should build gkENGINE manmally.
 echo -------------------------------------------------------------------------
