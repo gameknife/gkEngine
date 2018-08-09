891d7c75b975b9e2ebe1b02d335c34795004e01e
gameKnife <kaimingyi@163.com>
2018-08-09 21:05:28 +0800

    fix issue build on xcode9.4

---
+++ code/engine/xcenv.sh
@@ -0,0 +1,6 @@
+export BUILT_PRODUCTS_DIR="/Users/gameknife/github/gkEngine/exec/ios/Release-OSX"
+export CURRENT_ARCH="x86_64"
+export OBJECT_FILE_DIR_normal="/Users/gameknife/github/gkEngine/intermediate/Release-OSX/gkLauncherMac.build/Objects-normal"
+export OBJROOT="/Users/gameknife/github/gkEngine/intermediate/Release-OSX"
+export PRECOMPS_INCLUDE_HEADERS_FROM_BUILT_PRODUCTS_DIR="YES"
+export SRCROOT="/Users/gameknife/github/gkEngine/code/engine"
